bd74f7270fa47a4a7da3bd48fa53e7c7bde76316
ChaosOfThe4 <89749221+ChaosOfThe4@users.noreply.github.com>
2025-07-01 21:53:38 -0400

    Add files via upload

---
+++ Wesnoth Internals/Wesnoth Caving/Wesnoth Caving/dllmain.cpp
@@ -0,0 +1,51 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include <Windows.h>
+#include "pch.h"
+
+DWORD ret_addy = 0xCCAF90;
+DWORD* player_base;
+DWORD* game_base;
+DWORD* gold;
+
+
+
+__declspec(naked) void codecave() {
+	//assembly block to push everything onto the stack
+	__asm {
+		pushad
+	}
+
+
+	player_base = (DWORD*)0x017EED18; //player base in wesnoth
+	game_base = (DWORD*)(*player_base + 0xA90); // game base in wesnoth
+	gold = (DWORD*)(*game_base + 4); //gold location wesnoth
+	*gold = 999; //changes gold
+
+	//assembly block to pop everything back off the stack and run the original commands and return to where we placed our jmp for our code cave
+	__asm {
+		popad
+		mov eax, dword ptr ds : [ecx]
+		lea esi, dword ptr ds : [esi]
+		jmp ret_addy
+	}
+
+}
+
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+	DWORD old_protect;//Used for VirtualProtect
+	unsigned char* hook_location = (unsigned char*)0x00CCAF8A; //Unsigned char* so we can adjust the individual bytes
+
+	if (fdwReason == DLL_PROCESS_ATTACH) {
+
+		VirtualProtect((void*)hook_location, 6, PAGE_EXECUTE_READWRITE, &old_protect); //VirtualProtect to make executable memory writeable also and keep it executable
+
+		*hook_location = 0xE9; //opcode for jmp command in assemlby 
+		*(DWORD*)(hook_location + 1) = (DWORD)&codecave - ((DWORD)hook_location + 5); // Caculate the location using the formula: new_location - original_location+5
+		*(hook_location + 5) = 0x90; // Finally, since the original instructions totalled 6 bytes, NOP out the last remaining byte
+		MessageBox(0, 0, 0, 0); //Empty error box to show proper injection/DLL loading
+	}
+
+	return true;
+}
+
